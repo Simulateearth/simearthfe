2bc3ad35624bb5b2ac7ceb7b634f29a1cfe50af6
Jonas Felix <jonas.felix@felixideas.ch>
2017-07-09 16:18:26 +0200

    test for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,16 +21,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'sme'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('sme');
-  }));
-
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to sme!!');
+    expect(compiled.querySelector('h1').textContent).toContain('Simulate Earth');
   }));
 });

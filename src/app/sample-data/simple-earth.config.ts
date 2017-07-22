a06c5d2abca66a646d0b390e213a334b00fe092b
Jonas Felix <jonas.felix@felixideas.ch>
2017-07-22 18:53:51 +0200

    added deforestation simulation

--- src/app/sample-data/simple-earth.config.ts
+++ src/app/sample-data/simple-earth.config.ts
@@ -1,38 +1,46 @@
 export const simpleEarthConfig = {
-        interval: 300,
-        state: {
-          currentStep: 0,
-          population: 7500000000,
-          dailyDeathRate: 0.999979787,
-          dailyBirthRate: 1.000048,
-          meteoriteProbability: 1000 /* 1 of x days */,
-          meteoriteDeathRate: 0.5,
-          meteoriteRisingProbabilityRate: 0.99
-        },
-        effects: [
-          {
-            title: 'death',
-            change: 'population',
-            factorReference: 'dailyDeathRate',
-            probability: 1
-          },
-          {
-            title: 'birth',
-            change: 'population',
-            factorReference: 'dailyBirthRate',
-            probability: 1
-          },
-          {
-            title: 'meteorite',
-            change: 'population',
-            factorReference: 'meteoriteDeathRate',
-            probabilityReference: 'meteoriteProbability'
-          },
-          {
-            title: 'more meteorites',
-            change: 'meteoriteProbability',
-            factorReference: 'meteoriteRisingProbabilityRate',
-            probability: 1
-          }
-        ]
-      };
+  interval: 300,
+  state: {
+    currentStep: 0,
+    population: 7500000000,
+    dailyDeathRate: 0.999979787,
+    dailyBirthRate: 1.000048,
+    meteoriteProbability: 1000 /* 1 of x days */,
+    meteoriteDeathRate: 0.5,
+    meteoriteRisingProbabilityRate: 0.99,
+    forestSurface: 39991000,
+    deforestationRate: 0.999997649
+  },
+  effects: [
+    {
+      title: 'death',
+      change: 'population',
+      factorReference: 'dailyDeathRate',
+      probability: 1
+    },
+    {
+      title: 'birth',
+      change: 'population',
+      factorReference: 'dailyBirthRate',
+      probability: 1
+    },
+    {
+      title: 'meteorite',
+      change: 'population',
+      factorReference: 'meteoriteDeathRate',
+      probabilityReference: 'meteoriteProbability'
+    },
+    {
+      title: 'more meteorites',
+      change: 'meteoriteProbability',
+      factorReference: 'meteoriteRisingProbabilityRate',
+      probability: 1
+    },
+    {
+      title: 'deforestation',
+      change: 'forestSurface',
+      factorReference: 'deforestationRate',
+      probability: 1
+    }
+  ]
+};
